c1005532a0e84d6ef60bfadae6c478b108a1ff9b
tay_meow <taytayhedrick@gmail.com>
2020-02-03 20:54:59 -0500

    adding project from web

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share(){
+    window.alert('The product has been shared!');
+  }
+
+  onNotify(){
+    window.alert('You will be notfied when the product is on sale!');
+  }
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
